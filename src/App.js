d5a10472f5b2b0e173fdc3f6934029e67e25a35e
Waltsu <waltsumarcus@gmail.com>
2022-10-10 22:13:31 +0300

    yhdistys bakkariin

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import axios from "axios"
 
 
 const App = () => {
 
-
+  axios.get('http://localhost:3001').then(response => {
+    console.log(response);
+}).catch(e => {
+    console.log(e);
+});
 
   return (
     <div>
