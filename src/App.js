41f4be0100d533ce3ceaf91b93b1041b932ad77b
Waltsu <waltsumarcus@gmail.com>
2022-10-11 12:46:04 +0300

    material ui kirjasto yhdistetty

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import { useEffect, useState } from "react";
 import React from 'react';
 import { If, Else } from 'react-if';
 import parse from 'html-react-parser';
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
 
 const App = () => {
 
@@ -16,27 +25,49 @@ const App = () => {
 });
  },[])
 
+ const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
+
+ const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
 
 
   return (
     <div>
+      
       <If condition={foods.length === 0}>
         <h2>Ruokia haetaan......</h2>
       </If>
       <Else>
-      <div>
+      <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 700 }} aria-label="customized table">
+      <TableBody>
       {foods.map(food => {
-        
         return (
-          <div key={food.title}>
-          
-          {parse(food.description)}
-          <hr />
-          </div>
-          
-        );
-      })}
-    </div>
+          <StyledTableRow key={food.title}>
+         <StyledTableCell component="th" scope="row">
+                {food.title}
+              </StyledTableCell>
+              <StyledTableCell align="center">{parse(food.description)}</StyledTableCell>
+          </StyledTableRow> 
+        )})}
+      </TableBody>
+      </Table>
+     </TableContainer>
       </Else>
     </div>
   )
